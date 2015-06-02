bc27d1a5fda5c0f0b93ccf4c7c5cb64070b46cbb
mse2321 <mse233@gmail.com>
2015-06-02 19:57:03 -0400

    started Angular setup

---
+++ js/about.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+angular.module('me-innovation', [])
+	.controller('contactController')
--- js/me_innovation.js
+++ js/me_innovation.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+angular.module('me-innovation', ['ngRoute'])
+	.config(['$routeProvider',
+	  function($routeProvider) {
+	    $routeProvider.
+	      when('/about', {
+	        templateUrl: 'about.html',
+	        controller: 'aboutController'
+	      }).
+	      when('/contact', {
+	        templateUrl: 'contact.html',
+	        controller: 'contactController'
+	      }).
+	      otherwise({
+	        redirectTo: '/phones'
+	      });
+	  }]);
