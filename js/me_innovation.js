7d15c86160f42963f4a1154b95fcc992aac3f73b
mse2321 <mse233@gmail.com>
2015-06-09 19:55:49 -0400

    fixed issues with jumboimage

--- js/me_innovation.js
+++ js/me_innovation.js
@@ -3,20 +3,20 @@ angular.module('me-innovation', ['ngRoute'])
 	.config(['$routeProvider',
 	  function($routeProvider) {
 	    $routeProvider.
-	    /*  when('/about', {
+	    when('/about', {
 	        templateUrl: 'about.html',
 	        controller: 'aboutController'
-	      }). */
+	      }).
 	      when('/contact', {
-	        templateUrl: '../contact.html',
+	        templateUrl: 'contact.html',
 	        controller: 'contactController'
 	      }).
 	      when('/projects', {
-	        templateUrl: '../projects.html',
+	        templateUrl: 'projects.html',
 	        controller: 'projectsController'
 	      }).
 	      when('/index', {
-	        templateUrl: '../jumbotron.html',
+	        templateUrl: 'jumbotron.html',
 	        controller: 'jumboController'
 	      }).
 	      otherwise({
