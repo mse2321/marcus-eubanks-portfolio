0a4f21fc9d3817282b4f479a6ae4c6c92e724939
mse2321 <mse233@gmail.com>
2015-08-04 22:14:06 -0400

    updated some angular stuff, but not working

--- public/js/contact/contact.js
+++ public/js/contact/contact.js
@@ -1,4 +1,23 @@
-angular.module('me-innovation')
-	.controller('contactController', function($scope){
-		
-    });
+(function () {
+    'use strict';
+
+    angular.module('me-innovation').controller('ContactController', ['ContactService', contactController]);
+
+    function contactController(ContactService) {
+        // Grab a reference to keyword this in a more meaningful variable such as vm standing for view model
+        var vm = this;
+        /**
+         * Native and bindable properties
+         */
+        vm.contact = {};
+        /**
+         * Operations
+         */
+        vm.sendMail = function () {
+            ContactService.sendMail(vm.contact).success(function (response) {
+                // Clean current contact form
+                vm.contact = {};
+            });
+        };
+    }
+}());
