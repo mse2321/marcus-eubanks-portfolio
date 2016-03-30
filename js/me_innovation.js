e9b7a5b26cb6bad32e87b2cac520898ff178231f
mse2321 <mse233@gmail.com>
2016-03-29 23:24:13 -0400

    put in the final updates for now

--- js/me_innovation.js
+++ js/me_innovation.js
@@ -57,9 +57,9 @@ var demo = angular.module("demo", ['hmTouchEvents', 'ngRoute']);
 
 		$scope.items = [
 			{title: "Countries & Capitals", tech: "HTML5, CSS3, JavaScript, Angular, Geonames API", desc: "Thinkful Angular API Project", link: "http://mse2321.github.io/countries_capitals/index.html#/", image: "build/img/sample-image1.jpg"},
-			{title: "Muzak", tech: "HTML5, CSS3, JavaScript, jQuery, BootStrap, Angular, Spotify API", desc: "Thinkful Front End Course Capstone Project", link: "http://mse2321.github.io/muzak/", image: "build/img/sample-image2.jpg"},
+			{title: "Muzak", tech: "HTML5, CSS3, JavaScript, jQuery, BootStrap, Angular, Spotify API", desc: "Thinkful Front End Course Capstone Project w/ custom player", link: "http://mse2321.github.io/muzak/", image: "build/img/sample-image2.jpg"},
 			{title: "Basketball Quiz", tech: "HTML5, CSS3, JavaScript, jQuery, BootStrap", desc: "Thinkful Front End Course Project", link: "http://mse2321.github.io/basketball-quiz/", image: "build/img/sample-image3.jpg"},
-			{title: "XP Calculator", tech: "HTML5, CSS3, JavaScript, jQuery Mobile, Angular", desc: "Personal Project - RPG Experience Calculator", link: "http://www.meinnovation.net/revyntools/xp-calculator.html", image: "build/img/sample-image4.jpg"}
+			{title: "XP Calculator", tech: "HTML5, CSS3, JavaScript, jQuery Mobile, Angular, Angular-Material", desc: "Personal Project - RPG Experience Calculator", link: "http://www.meinnovation.net/revyntools/xp-calculator.html", image: "build/img/sample-image4.jpg"}
 		];
 			
 		$scope.defaultIndex = 0;
